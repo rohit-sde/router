5c2674b44e93d493a93c3f7db2c35bfbf2a5fa79
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-14 17:29:27 +0530

    get homepage to index route

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <RootLayout />,
         errorElement: <Error />,
         children: [
-            { path: "/", element: <HomePage /> },
+            { index: true, element: <HomePage /> },
             { path: "/products", element: <ProductsPage /> },
             { path: "/products/:productId", element: <ProductDetailPage /> },
         ],
